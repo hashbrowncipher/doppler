9dfd3a34aaea83f4f716c3131375bf3399bcb8b7
Josh Snyder <josh@code406.com>
2013-03-21 15:53:11 -0700

    Convert to an iterator

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ from math import cos, pi
 speed_of_sound = 343 # meters/second
 sample_frequency = 48000 # samples/second
 # stop tracking emitted sounds after window_length seconds
-window_length = 0.001
+window_length = 0.5
 
 sample_length = 1.0 / sample_frequency
 samples_by_distance = sample_frequency / speed_of_sound # samples/meter
@@ -11,7 +11,7 @@ bucket_count = int(sample_frequency * window_length)
 
 tau = 2 * pi
 
-def distance(a, b):
+def distancesq(a, b):
     return sum(map(lambda (x, y): (x - y)**2, zip(a, b)))**0.5 
 
 def simple_cos(f):
@@ -25,32 +25,36 @@ class NerfSimulator(object):
     def blank_bucket(self):
         return [0]*len(self.mics)
 
-    def __init__(self):
+    def __init__(self, position, velocity, signal):
         self.buckets = [self.blank_bucket() for i in xrange(bucket_count)]
-        self.window_start = 0
+        self.time = 0
+
+        self.position = position
+        self.velocity = velocity
+        self.signal = signal
 
     def emit(self, position, pressure):
         for i in xrange(self.mic_count):
-            target_bucket = int(distance(position, self.mics[i]) * samples_by_distance)
-            print target_bucket
+            d2 = distancesq(position, self.mics[i])
+            target_bucket = int(d2**0.5 * samples_by_distance)
             if target_bucket < bucket_count:
-                actual_bucket = (self.window_start + target_bucket) % bucket_count
-                self.buckets[actual_bucket][i] += pressure
-        print self.buckets
+                actual_bucket = (self.time + target_bucket) % bucket_count
+                self.buckets[actual_bucket][i] += pressure / d2
 
     def finalize_bucket(self):
         # after a certain time it will be impossible for the dart to further influence the bucket
         # (the sound can't travel back in time, after all)
-        ret = self.buckets[self.window_start]
-        self.buckets[self.window_start]=self.blank_bucket()
-        self.window_start = (self.window_start + 1) % bucket_count
+        window_start = self.time % bucket_count
+        ret = self.buckets[window_start]
+        self.buckets[window_start] = self.blank_bucket()
+        self.time += 1
         return ret
 
-    def throw_dart(self, position_0, velocity, samples, signal):
-        position = position_0
-        for i in xrange(samples):
-            pressure = signal(i)
-            self.emit(position, signal(i))
-            yield self.finalize_bucket()
-            displacement = [i*sample_length for i in velocity]
-            position = map(sum, zip(position, displacement))
+    def next(self):
+        pressure = self.signal(self.time)
+        self.emit(self.position, pressure)
+        displacement = [i*sample_length for i in self.velocity]
+        self.position = map(sum, zip(self.position, displacement))
+        return self.finalize_bucket()
+
+#a = NerfSimulator((-1, -0.25, 0), (5, 0, 0), simple_cos(2000))
