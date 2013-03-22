8bbb1063cd0ebfa50a29bf1e344979c74ab34fee
David Selassie <dselassi@yelp.com>
2013-03-21 22:35:07 -0700

    Makes multilateration works again.

--- multilateration.py
+++ multilateration.py
@@ -6,12 +6,14 @@ from numpy import random
 from numpy import concatenate
 from numpy import sum
 from numpy import mean
-from numpy.linalg import solve
 from numpy.linalg.linalg import pinv
 from scipy.spatial.distance import euclidean
 from scipy.spatial.distance import cdist
 from scipy.spatial.distance import pdist
 from scipy.spatial.distance import squareform
+
+from pipe_util import split_fileinput
+from pipe_util import join_output
 
 
 mics = array([
@@ -25,9 +27,7 @@ mics = array([
 ])
 fake_dart = array([1.5, 2, 0.5])
 
-mic_mic_distances = squareform(pdist(mics, 'euclidean'))
 mic_dart_distances = array([euclidean(mic, fake_dart) for mic in mics])
-mic_dart_distances += random.normal(0.0, 0.1, mic_dart_distances.shape)
 
 
 def transpose_1D(M):
@@ -45,18 +45,37 @@ def multilaterate(mic_positions, mic_dart_distances_stream):
 	"""
 	mic_positions = array(mic_positions)
 	origin = mic_positions[0]
-	mic_positions -= origin
+	mic_positions = mic_positions - origin
 
 	for mic_dart_distances in mic_dart_distances_stream:
 		mic_dart_distances = array(mic_dart_distances)
 
-		vt = mic_dart_distances[2:] - mic_dart_distances[0]
+		vt = mic_dart_distances - mic_dart_distances[0]
+		free_vt = vt[2:]
 
-		A = 2.0 * mic_positions[2:, 0] / vt - 2.0 * mic_positions[1, 0] / vt[1]
-		B = 2.0 * mic_positions[2:, 1] / vt - 2.0 * mic_positions[1, 1] / vt[1]
-		C = 2.0 * mic_positions[2:, 2] / vt - 2.0 * mic_positions[1, 2] / vt[1]
-		D = vt - vt[1] - sum(mic_positions[2:, :] ** 2, axis=1) / vt + sum(mic_positions[1] ** 2) / vt[1]
+		A = 2.0 * mic_positions[2:, 0] / free_vt - 2.0 * mic_positions[1, 0] / vt[1]
+		B = 2.0 * mic_positions[2:, 1] / free_vt - 2.0 * mic_positions[1, 1] / vt[1]
+		C = 2.0 * mic_positions[2:, 2] / free_vt - 2.0 * mic_positions[1, 2] / vt[1]
+		D = free_vt - vt[1] - sum(mic_positions[2:, :] ** 2, axis=1) / free_vt + sum(mic_positions[1] ** 2) / vt[1]
 
 		M = concatenate([transpose_1D(A), transpose_1D(B), transpose_1D(C)], axis=1)
 
 		yield pinv(M).dot(-transpose_1D(D)).reshape(3) + origin
+
+
+if __name__ == '__main__':
+	CHANNELS = 8
+
+	real_mics = array([
+		[1.0, 0.0, 0.0],
+		[0.0, 3.0, 0.0],
+		[0.0, 0.0, 2.0],
+		[1.0, 1.0, 1.0],
+		[4.0, 2.0, 4.0],
+		[0.0, 0.0, 9.0],
+		[5.0, 2.0, -2.0],
+		[0.0, 2.0, 3.0],
+	])
+
+	for coordinates in multilaterate(real_mics, split_fileinput([float] * CHANNELS)):
+		join_output(coordinates)
