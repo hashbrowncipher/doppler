a3b99e68ff1a93857f482dd866092db6e6655dda
David Selassie <dselassi@yelp.com>
2013-03-22 12:06:21 -0700

    Does dropped event detection in alignment. Multilateration doesn't support this output yet, though.

--- alignment.py
+++ alignment.py
@@ -6,31 +6,81 @@ from pipe_util import split_fileinput
 from pipe_util import join_output
 from world_params import CHANNEL_COUNT
 from world_params import SPEED_OF_SOUND_METERS_SECOND
+from world_params import MAX_MIC_DELAY_SECONDS
 
 
-def align(event_stream):
-	"""Assume that every microphone will see every peak. Then each microphone
-	will see the same number of peaks arriving in the same order (unless the
-	dart is faster than sound). So have some queues of events from every mic,
-	then ouput the times of the same event arriving at every mic.
+def first(l):
+	"""Return the first non-None object in l."""
+	for i in l:
+		if i is not None:
+			return i
+
+
+def detect_missed_event(current_time_seconds, time_seconds_queues,
+	threshold_seconds):
+	"""If the current time is greater than the maximum delay away from any of
+	the currently aligned events, then we've missed an event."""
+	for time_seconds_queue in time_seconds_queues:
+		if (len(time_seconds_queue) > 0
+			and current_time_seconds - time_seconds_queue[0] >= threshold_seconds):
+			return True
+	return False
+
+
+def times_to_time_distances(times_seconds):
+	"""Takes a tuple of times (possibly with some Nones) and returns the tuple
+	of time, (distances)."""
+	key_time_seconds = first(times_seconds)
+	distances_meters = [
+		time_seconds * SPEED_OF_SOUND_METERS_SECOND if time_seconds is not None else None
+		for time_seconds
+		in times_seconds
+	]
+	return key_time_seconds, distances_meters
+
+
+def align(channel__event_time_seconds_stream, allow_dropped_events=True):
+	"""Each microphone should see the same number of peaks arriving in the
+	same order (unless the dart is faster than sound!). So have some queues of
+	events from every mic, then ouput the times of the same event arriving at
+	every mic.
 
 	event_stream must be a generator of (channel int, time float).
 	Yields time + 8-tuples of distances.
 	"""
-	queues = tuple(deque() for i in range(CHANNEL_COUNT))
+	time_seconds_queues = tuple(deque() for i in range(CHANNEL_COUNT))
 
-	for channel, event_t in event_stream:
+	for channel, event_time_seconds in channel__event_time_seconds_stream:
 		# In on the right.
-		queues[channel].append(event_t)
+		time_seconds_queues[channel].append(event_time_seconds)
+
+		# Check to see if any peaks could be missing at this point. This
+		# should be detectable as an empty channel queue.
+		if (allow_dropped_events and
+			detect_missed_event(
+				event_time_seconds,
+				time_seconds_queues,
+				MAX_MIC_DELAY_SECONDS
+			)):
+			for time_seconds_queue in time_seconds_queues:
+				if len(time_seconds_queue) < 1:
+					time_seconds_queue.append(None)
 
 		# If we've seen a peak on every channel,
-		if all(len(queue) > 0 for queue in queues):
+		if all(
+			len(time_seconds_queue) > 0
+			for time_seconds_queue
+			in time_seconds_queues
+		):
 			# yield all those relative times.
-			# Out on the left.
-			yield tuple(queue.popleft() for queue in queues)
+			yield times_to_time_distances(tuple(
+				# Out on the left.
+				time_seconds_queue.popleft()
+				for time_seconds_queue
+				in time_seconds_queues
+			))
 
 
 if __name__ == '__main__':
-	for aligned_times in align(split_fileinput((int, float))):
-		aligned_distances = array(aligned_times) * SPEED_OF_SOUND_METERS_SECOND
-		join_output([aligned_times[0]] + list(aligned_distances))
+	for time_seconds, aligned_distances_meters in align(split_fileinput((int, float))):
+		join_output([time_seconds] + list(aligned_distances_meters))
