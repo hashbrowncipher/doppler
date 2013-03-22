f21b495b29bf73b3eb333bb25df908add9e45656
David Selassie <dselassi@yelp.com>
2013-03-21 23:37:19 -0700

    Adds SAMPLE_RATE_HERTZ.

--- world_params.py
+++ world_params.py
@@ -1,8 +1,9 @@
 from numpy import array
 
 
+SAMPLE_RATE_HERTZ = 48000
 CHANNELS = 8
-SPEED_OF_SOUND_METERS_SECOND = 340.29 # m / sec
+SPEED_OF_SOUND_METERS_SECOND = 340.29
 
 MIC_COORDS = array([
 	[0.0, 0.0, 0.0],
