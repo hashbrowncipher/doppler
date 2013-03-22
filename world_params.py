7d57c0fe75c50b7bc92b8023ac429d1bccc3a2cb
David Selassie <dselassi@yelp.com>
2013-03-22 10:51:08 -0700

    Adds max delay estimation.

--- world_params.py
+++ world_params.py
@@ -1,4 +1,5 @@
 from numpy import array
+from scipy.spatial.distance import pdist
 
 
 SAMPLE_RATE_HERTZ = 48000
@@ -16,3 +17,9 @@ MIC_COORDS = array([
 	[1.0, 0.0, 1.0],
 	[1.0, 1.0, 1.0],
 ])
+
+# Figure out a threshold for the maximum difference in delays we should
+# observe.
+MIC_MIC_DISTANCES = pdist(MIC_COORDS, 'euclidean')
+DELAY_FUDGE_FACTOR = 1.1
+MAX_MIC_DELAY_SECONDS = max(MIC_MIC_DISTANCES) / SPEED_OF_SOUND_METERS_SECOND * DELAY_FUDGE_FACTOR
