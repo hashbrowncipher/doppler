fd8d3d7789ab36b555b50a9c6651ed53980e3386
David Selassie <dselassi@yelp.com>
2013-03-21 21:45:15 -0700

    Renames events to event_stream in align.

--- alignment.py
+++ alignment.py
@@ -2,18 +2,18 @@ from collections import deque
 
 CHANNELS = 8
 
-def align(events):
+def align(event_stream):
 	"""Assume that every microphone will see every peak. Then each microphone
 	will see the same number of peaks arriving in the same order (unless the
 	dart is faster than sound). So have some queues of events from every mic,
 	then ouput the times of the same event arriving at every mic.
 
-	Events must be a generator of (channel int, time float).
+	event_stream must be a generator of (channel int, time float).
 	Yields 8-tuples of time floats.
 	"""
 	queues = tuple(deque() for i in range(CHANNELS))
 
-	for channel, event_t in events:
+	for channel, event_t in event_stream:
 		# In on the right.
 		queues[channel].append(event_t)
 
